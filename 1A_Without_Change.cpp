44543b1e602bb391e9d77a2e9baed43e13f65ded
Eldawn <omareldawn@gmail.com>
2025-08-05 07:34:02 +0300

    Update 1A_Without_Change.cpp

--- 1A_Without_Change.cpp
+++ 1A_Without_Change.cpp
@@ -2,25 +2,28 @@
 #include<iostream>
 #include <numeric>
 using namespace std;
+// Problem: http://codeforces.com/contest/732/problem/A
+// Tags: Implementation, Brute force
 
 int main() {
 int n,x;
     cin>>n>>x;
-    int m=n;
-    int count=0;
-    int cha = n%10;
+    // n = price of one shove, x = value of special coin.
+    int m=n; //stores original price of the shovel.
+    int count=0; //bought shovels count.
     bool X=true;
     while (X) {
+        // checks if the last digit is zero or matches the special coin.
        if (n%10==x || n%10==0) {
-            count++;
-            X=false;
+            count++; //adds last shovel.
+            X=false; //terminates the loop.
         }
         else {
-            n+=m;
-            count++;
+            n+=m; //buy one more shovel (Add the prices up)
+            count++; //increment the number of shovels.
         }
 
     }
-    cout<<count<<endl;
+    cout<<count<<endl; //desired output
     return 0;
 }
